c63c036e0bd51965bae6cf0093198db2041eda21
ViktoriaMityaeva <mitiaeva.viktoria2014@yandex.ru>
2022-04-02 17:41:21 +0300

    Create Button_Middle.cs

---
+++ Scripts/Buttons/Button_Middle.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Middle : MonoBehaviour
+{
+    public Camera_Trans1 Begin;
+    void Start(){
+        gameObject.SetActive(false);     
+    }
+}
