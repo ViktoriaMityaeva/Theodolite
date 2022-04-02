af17ba236c901ea0d6b67a1758f6d42b5bf8cd2a
ViktoriaMityaeva <mitiaeva.viktoria2014@yandex.ru>
2022-04-02 17:42:26 +0300

    Create Gradus2.cs

---
+++ Scripts/Gradus2.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gradus2 : MonoBehaviour
+{
+    public float x;
+    public float y;
+    public Text tt;
+
+    void Update(){
+        int GV = 180;
+        x = ((transform.position.x * 10000000 + 1548706) / 320); 
+        x = Mathf.Round(x);
+
+        if (x < 0){
+            GV = 181;
+            tt.text = GV.ToString();
+            x = x * (-1);
+            while (x >= 60){
+                x = x - 60;
+                GV = GV + 1;
+                tt.text = GV.ToString();
+            }
+            if (x < 60)
+                y = x;
+        }
+
+        if (x >= 0){
+            tt.text = GV.ToString();
+            while (x >= 60){
+                x = x - 60;
+                GV = GV - 1;
+                tt.text = GV.ToString();
+            }
+            if (x < 60)
+                y = x;
+        }
+    }
+}
